01271eff5c2b0d0b9b20768cab9f11296dd67e35
pattarapolwnr <pattarapol.wangnirun@gmail.com>
2024-04-19 13:27:04 +0700

    config: edit hostname in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    baseURL:
-      "https://ec2-54-206-21-106.ap-southeast-2.compute.amazonaws.com:443",
+    baseURL: "https://api.pattarapolw.com",
   },
 };
 
